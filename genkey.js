323ab443377af5a586eba687a6f94d78dc4e6d8d
Ben Lu <ben@psp-power.com.tw>
2019-04-08 14:38:06 +0800

    Fix private key to address

--- genkey.js
+++ genkey.js
@@ -2,4 +2,4 @@ let Web3 = require('web3')
 let web3 = new Web3('http://localhost:8545')
 
 let account = web3.eth.accounts.create()
-console.log(account.privateKey.substring(2, account.privateKey.length))
+console.log(account.privateKey)
