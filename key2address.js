7b437369102b9c4eb54ea73fb9324dbd5b309491
Ben Lu <ben@psp-power.com.tw>
2019-04-08 14:07:44 +0800

    Fix args

--- key2address.js
+++ key2address.js
@@ -1,5 +1,7 @@
 let Web3 = require('web3')
 let web3 = new Web3('http://localhost:8545')
 
-let account = web3.eth.accounts.privateKeyToAccount("855572a12d3bfacc7f9bc7004a11c0dd7f5fc58d9c03e40e9c3021cc40df8ade")
+let argv = process.argv;
+
+let account = web3.eth.accounts.privateKeyToAccount(argv[2])
 console.log(account.address)
